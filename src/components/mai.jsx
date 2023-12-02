769b875ff1fc3b21be92a76a955db36feaca8c80
M-Ali-Haider <mah01134212100@gmail.com>
2023-12-02 23:42:08 +0500

    created tags

--- src/components/mai.jsx
+++ src/components/mai.jsx
@@ -1,9 +1,10 @@
 import '../assets/styles/main.css'
+import Tags from './tags';
 const Main=()=>{
     return(
         <>
         <div className="main">
-            
+            <Tags />
         </div>
         </>
     )
---
+++ src/components/tags.jsx
@@ -0,0 +1,23 @@
+import '../assets/styles/tags.css'
+const Tags=()=>{
+    return(
+        <>
+        <div className="tags-header">
+            <div className="tags-unit tags-unit-active">All</div>
+            <div className="tags-unit">Music</div>
+            <div className="tags-unit">Action Thrillers</div>
+            <div className="tags-unit">Siraiki Cinema</div>
+            <div className="tags-unit">Thrillers</div>
+            <div className="tags-unit">News</div>
+            <div className="tags-unit">Comedy</div>
+            <div className="tags-unit">Pakistani dramas</div>
+            <div className="tags-unit">Live</div>
+            <div className="tags-unit">Wickets</div>
+            <div className="tags-unit">Gaming</div>
+            <div className="tags-unit">History</div>
+            <div className="tags-unit">Anime</div>
+        </div>
+        </>
+    )
+}
+export default Tags;
