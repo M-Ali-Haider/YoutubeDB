44cd2ae8d4e2cbf52244188c566370745871e9d8
M-Ali-Haider <mah01134212100@gmail.com>
2023-12-03 17:15:43 +0500

    bruhmius

--- src/components/tags.jsx
+++ src/components/tags.jsx
@@ -1,23 +1,67 @@
 import '../assets/styles/tags.css'
+import next from '../assets/images/next.svg'
+import prev from '../assets/images/prev.svg'
+import {Swiper,SwiperSlide} from 'swiper/react'
+import 'swiper/css'
+import 'swiper/css/navigation'
+import {Navigation} from 'swiper/modules'
+import SwiperCore from 'swiper/core'
+SwiperCore.use([Navigation])
+
 const Tags=({isSidebarOpen})=>{
+    const handleSlideChange = (swiper) => {
+        const currentIndex = swiper.activeIndex;
+        const isFirstSlide = currentIndex === 0;
+        const tagEmpPrevElement = document.querySelector('.tag-emp-prev');
+        if (tagEmpPrevElement) {
+          tagEmpPrevElement.style.display = isFirstSlide ? 'none' : 'block';
+        }
+      };
     return(
         <>
         <div className="tags-container">
             <div className={`tags-helper ${isSidebarOpen ? 'tags-helper-active' : ''}`}></div>
             <div className="tags-header">
-                <div className="tags-unit tags-unit-active">All</div>
-                <div className="tags-unit">Music</div>
-                <div className="tags-unit">Action Thrillers</div>
-                <div className="tags-unit">Siraiki Cinema</div>
-                <div className="tags-unit">Thrillers</div>
-                <div className="tags-unit">News</div>
-                <div className="tags-unit">Comedy</div>
-                <div className="tags-unit">Pakistani dramas</div>
-                <div className="tags-unit">Live</div>
-                <div className="tags-unit">Wickets</div>
-                <div className="tags-unit">Gaming</div>
-                <div className="tags-unit">History</div>
-                <div className="tags-unit">Anime</div>
+                <Swiper
+                    navigation={{ nextEl: '.tag-next', prevEl: '.tag-prev' }}
+                    slidesPerView={10}
+                    onSlideChange={handleSlideChange}
+                    className='tagsSwiper'
+                >
+                    <SwiperSlide className="tags-unit tags-unit-active">All</SwiperSlide>
+                    <SwiperSlide className="tags-unit">Music</SwiperSlide>
+                    <SwiperSlide className="tags-unit">Action Thrillers</SwiperSlide>
+                    <SwiperSlide className="tags-unit">Siraiki Cinema</SwiperSlide>
+                    <SwiperSlide className="tags-unit">Thrillers</SwiperSlide>
+                    <SwiperSlide className="tags-unit">News</SwiperSlide>
+                    <SwiperSlide className="tags-unit">Comedy</SwiperSlide>
+                    <SwiperSlide className="tags-unit">Pakistani dramas</SwiperSlide>
+                    <SwiperSlide className="tags-unit">Live</SwiperSlide>
+                    <SwiperSlide className="tags-unit">Wickets</SwiperSlide>
+                    <SwiperSlide className="tags-unit">Gaming</SwiperSlide>
+                    <SwiperSlide className="tags-unit">History</SwiperSlide>
+                    <SwiperSlide className="tags-unit">Anime</SwiperSlide>
+                    <SwiperSlide className="tags-unit">Romance</SwiperSlide>
+                    <SwiperSlide className="tags-unit">Football</SwiperSlide>
+                    <SwiperSlide className="tags-unit">Boqir</SwiperSlide>
+                    
+                    <div className="tag-next-div">
+                        <div className="tag-emp-next"></div>
+                        <div className="tag-next-image">
+                            <div className="tag-next">
+                                <img src={next} alt="" />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="tag-prev-div">
+                        <div className="tag-prev-image">
+                            <div className="tag-prev">
+                                <img src={prev} alt="" />
+                            </div>
+                        </div>
+                        <div className="tag-emp-prev"></div>
+                    </div>
+                </Swiper>
             </div>
         </div>
         </>
